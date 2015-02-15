6e6f0e8489692d41bee6d7a269f1295b2aa5cf4c
Julian <saucecode@users.noreply.github.com>
2015-02-15 22:25:05 +1300

    re-fix text render orientation code

--- src/tools/FontTT.java
+++ src/tools/FontTT.java
@@ -373,16 +373,16 @@ public class FontTT {
 		// draw a quad with to place the character onto
 		GL11.glBegin(GL11.GL_QUADS);
 		{
-			GL11.glTexCoord2f(0, texheight);
+			GL11.glTexCoord2f(0, 0);
 			GL11.glVertex2f(0 + x, 0 - y);
 			
-			GL11.glTexCoord2f(0, 0);
+			GL11.glTexCoord2f(0, texheight);
 			GL11.glVertex2f(0 + x, imgheight - y);
 			
-			GL11.glTexCoord2f(texwidth, 0);
+			GL11.glTexCoord2f(texwidth, texheight);
 			GL11.glVertex2f(imgwidth + x,imgheight - y);
 			
-			GL11.glTexCoord2f(texwidth, texheight);
+			GL11.glTexCoord2f(texwidth, 0);
 			GL11.glVertex2f(imgwidth + x,0 - y);
 		}
 		GL11.glEnd();
